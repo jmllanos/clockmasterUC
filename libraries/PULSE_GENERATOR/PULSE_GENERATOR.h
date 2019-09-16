c7698f7353a3312444a745d125f6fd4ee4c72218
juan <llanosmirandajuan@gmail.com>
2019-09-16 08:11:55 -0500

    Creation of PULSE GENERATOR class

--- libraries/PULSE_GENERATOR/PULSE_GENERATOR.h
+++ libraries/PULSE_GENERATOR/PULSE_GENERATOR.h
@@ -1,28 +1,148 @@
 #ifndef _PULSE_GENERATOR_H_
 #define _PULSE_GENERATOR_H_
 
+#include<Arduino.h>
+#include<ClockMasterRegisters.h>
+#include<read_write_registers.h>
+#include<ArduinoJson.h>
 
-class pulse_gen
+class PulseGenerator
 {
     private:
-        int enable_addr;
-        int year_h_addr;
-        int year_l_addr;
-        int usr_month_addr;
-        int usr_day_addr;
-        int usr_minutes_addr;
-        int usr_seconds_addr;
-        int usr_width_high_3_addr;
-        int usr_width_high_2_addr;
-        int usr_width_high_1_addr;
-        int usr_width_high_0_addr;
-        int usr_width_period_3_addr;
-        int usr_width_period_2_addr;
-        int usr_width_period_1_addr;
-        int usr_width_period_0_addr;
+        byte enable_addr;
+        byte usr_year_h_addr;
+        byte usr_year_l_addr;
+        byte usr_month_addr;
+        byte usr_day_addr;
+        byte usr_hour_addr;
+        byte usr_minutes_addr;
+        byte usr_seconds_addr;
+        byte usr_width_high_3_addr;
+        byte usr_width_high_2_addr;
+        byte usr_width_high_1_addr;
+        byte usr_width_high_0_addr;
+        byte usr_width_period_3_addr;
+        byte usr_width_period_2_addr;
+        byte usr_width_period_1_addr;
+        byte usr_width_period_0_addr;
+        
+        byte enable;
+        byte year;
+        byte month;
+        byte day;
+        byte hour;
+        byte minutes;
+        byte seconds;
+        long width;
+        long width_period;
+        int channel;
+        
+        void set_registers();
+        
     public:
-        pulse_gen(int number);
+        void set_parameters();
+        void set_channel(int _channel);
+        
 
 };
+
+void PulseGenerator::set_channel(int _channel)
+{
+ channel=_channel;
+}
+
+void PulseGenerator::set_parameters()
+{    
+    byte response[3];
+    WRITE_REGISTER(enable_addr,enable,response);
+           
+}
+
+void PulseGenerator::set_registers()
+{
+  switch(channel)
+	  { 
+	  case 0:
+		enable_addr             =PG0_PULSE_ENA;
+		usr_year_h_addr         =PG0_USR_YEAR_H;
+		usr_year_l_addr         =PG0_USR_YEAR_L;
+		usr_month_addr          =PG0_USR_MONTH;
+		usr_day_addr            =PG0_USR_DAY;
+		usr_hour_addr           =PG0_USR_HOUR;
+		usr_minutes_addr        =PG0_USR_MINUTES;
+		usr_seconds_addr        =PG0_USR_SECONDS;
+		usr_width_high_3_addr   =PG0_WIDTH_HIGH_3;
+		usr_width_high_2_addr   =PG0_WIDTH_HIGH_2;
+		usr_width_high_1_addr   =PG0_WIDTH_HIGH_1;
+		usr_width_high_0_addr   =PG0_WIDTH_HIGH_0;
+		usr_width_period_3_addr =PG0_WIDTH_PERIOD_3;
+		usr_width_period_2_addr =PG0_WIDTH_PERIOD_2;
+		usr_width_period_1_addr =PG0_WIDTH_PERIOD_1;
+		usr_width_period_0_addr =PG0_WIDTH_PERIOD_0;
+	   break;
+	         
+	  case 1:
+		enable_addr             =PG1_PULSE_ENA;
+		usr_year_h_addr         =PG1_USR_YEAR_H;
+		usr_year_l_addr         =PG1_USR_YEAR_L;
+		usr_month_addr          =PG1_USR_MONTH;
+		usr_day_addr            =PG1_USR_DAY;
+		usr_hour_addr           =PG1_USR_HOUR;
+		usr_minutes_addr        =PG1_USR_MINUTES;
+		usr_seconds_addr        =PG1_USR_SECONDS;
+		usr_width_high_3_addr   =PG1_WIDTH_HIGH_3;
+		usr_width_high_2_addr   =PG1_WIDTH_HIGH_2;
+		usr_width_high_1_addr   =PG1_WIDTH_HIGH_1;
+		usr_width_high_0_addr   =PG1_WIDTH_HIGH_0;
+		usr_width_period_3_addr =PG1_WIDTH_PERIOD_3;
+		usr_width_period_2_addr =PG1_WIDTH_PERIOD_2;
+		usr_width_period_1_addr =PG1_WIDTH_PERIOD_1;
+		usr_width_period_0_addr =PG1_WIDTH_PERIOD_0;
+	   break;
+
+	  case 2:
+		enable_addr             =PG2_PULSE_ENA;
+		usr_year_h_addr         =PG2_USR_YEAR_H;
+		usr_year_l_addr         =PG2_USR_YEAR_L;
+		usr_month_addr          =PG2_USR_MONTH;
+		usr_day_addr            =PG2_USR_DAY;
+		usr_hour_addr           =PG2_USR_HOUR;
+		usr_minutes_addr        =PG2_USR_MINUTES;
+		usr_seconds_addr        =PG2_USR_SECONDS;
+		usr_width_high_3_addr   =PG2_WIDTH_HIGH_3;
+		usr_width_high_2_addr   =PG2_WIDTH_HIGH_2;
+		usr_width_high_1_addr   =PG2_WIDTH_HIGH_1;
+		usr_width_high_0_addr   =PG2_WIDTH_HIGH_0;
+		usr_width_period_3_addr =PG2_WIDTH_PERIOD_3;
+		usr_width_period_2_addr =PG2_WIDTH_PERIOD_2;
+		usr_width_period_1_addr =PG2_WIDTH_PERIOD_1;
+		usr_width_period_0_addr =PG2_WIDTH_PERIOD_0;
+	   break;
+	  
+	  case 3:
+		enable_addr             =PG3_PULSE_ENA;
+		usr_year_h_addr         =PG3_USR_YEAR_H;
+		usr_year_l_addr         =PG3_USR_YEAR_L;
+		usr_month_addr          =PG3_USR_MONTH;
+		usr_day_addr            =PG3_USR_DAY;
+		usr_hour_addr           =PG3_USR_HOUR;
+		usr_minutes_addr        =PG3_USR_MINUTES;
+		usr_seconds_addr        =PG3_USR_SECONDS;
+		usr_width_high_3_addr   =PG3_WIDTH_HIGH_3;
+		usr_width_high_2_addr   =PG3_WIDTH_HIGH_2;
+		usr_width_high_1_addr   =PG3_WIDTH_HIGH_1;
+		usr_width_high_0_addr   =PG3_WIDTH_HIGH_0;
+		usr_width_period_3_addr =PG3_WIDTH_PERIOD_3;
+		usr_width_period_2_addr =PG3_WIDTH_PERIOD_2;
+		usr_width_period_1_addr =PG3_WIDTH_PERIOD_1;
+		usr_width_period_0_addr =PG3_WIDTH_PERIOD_0;
+	   break;
+	  
+	  
+	  }
+  
+}
+
+
 
 #endif
